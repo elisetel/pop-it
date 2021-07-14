46cd81c0d2de752a2e4594b4a72e8c83e46f16c8
Emil Telstad <emil.telstad@gmail.com>
2021-07-15 00:02:33 +0200

    matte test

---
+++ matte.py
@@ -0,0 +1,19 @@
+
+def pluss(a, b):
+    return a + b
+
+
+
+if __name__ == '__main__':
+    # emil = 24
+    # elise = 11
+    # input("Hva heter du? ")
+    # alder = pluss(emil, elise)
+    # print(f"Summen er {alder}")
+    
+    alder_emil = int( input("hvor gammel er emil? ") ) 
+    alder_elise = int( input("hvor gammel er elise? ") ) 
+    alder_begge = (alder_emil + alder_elise)
+    print(f"alderen av dere to fjompene, er {alder_begge}" )
+    
+    
